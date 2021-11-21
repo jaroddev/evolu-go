16be14421de54bff6ad6e15d5c513db4c526c204
jaroddev <martinjarod17@gmail.com>
2021-11-21 16:54:17 +0100

    Implemented most of what makes a genetic algorithm
    Still lacks any real crossover behavior or Survival to get rid of some old and low-fitness chromosomes.

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"sort"
+	"time"
 )
 
 type Chromosome struct {
@@ -13,13 +16,17 @@ type Chromosome struct {
 type Population []Chromosome
 
 type GA struct {
-	Pop   Population
+	Pop Population
+	// Best chromosome
+	Best  *Chromosome
 	Cycle int
+	// Last cycle the
+	LastUpdate int
 
 	Init func() Population
 	Fit  func(*Chromosome)
 
-	Stop func(*Population, int) bool
+	Stop func(*GA) bool
 
 	Select   func(*Population) Population
 	Cross    func(*Population) Population
@@ -28,6 +35,8 @@ type GA struct {
 }
 
 func (algorithm *GA) Run() {
+	// Always init cycle to zero
+	algorithm.Cycle = 1
 
 	algorithm.Pop = algorithm.Init()
 
@@ -35,14 +44,17 @@ func (algorithm *GA) Run() {
 		algorithm.Fit(&chromosome)
 	}
 
-	for algorithm.Stop(&algorithm.Pop, algorithm.Cycle) {
+	algorithm.Best = &algorithm.Pop[0]
+
+	for algorithm.Stop(algorithm) {
+		// Check which is the new best chromosome
 
 		parents := algorithm.Select(&algorithm.Pop)
 
 		children := algorithm.Cross(&parents)
 
-		for _, child := range children {
-			algorithm.Fit(&child)
+		for index := range children {
+			algorithm.Fit(&algorithm.Pop[index])
 		}
 
 		algorithm.Pop = append(algorithm.Pop, children...)
@@ -52,6 +64,115 @@ func (algorithm *GA) Run() {
 			return algorithm.Pop[j].Fitness < algorithm.Pop[i].Fitness
 		})
 
+		algorithm.Best = &algorithm.Pop[0]
+
+		algorithm.Survive(&algorithm.Pop)
+
+		algorithm.Cycle++
+
+		fmt.Println("number of individuals in population", len(algorithm.Pop))
+		fmt.Println("alleles of the best indiidual", algorithm.Best.Alleles)
+
+		fmt.Println("best fitness", algorithm.Best.Fitness)
 	}
 
+}
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+
+	algorithm := &GA{}
+
+	algorithm.Init = func() Population {
+		initPopLength := 2
+		alleleLength := 8
+
+		pop := make(Population, initPopLength)
+
+		for index := range pop {
+			pop[index].Alleles = make([]bool, alleleLength)
+
+			for locus := range pop[index].Alleles {
+				if rand.Intn(2) == 1 {
+					pop[index].Alleles[locus] = true
+				} else {
+					pop[index].Alleles[locus] = false
+				}
+			}
+		}
+
+		return pop
+	}
+
+	algorithm.Fit = func(c *Chromosome) {
+		c.Fitness = 0
+		for _, allele := range c.Alleles {
+			if allele {
+				c.Fitness++
+			}
+		}
+	}
+
+	algorithm.Stop = func(g *GA) bool {
+		// Return true if the algorithm should continue
+		// if return false then the algorithm stops
+		// TODO: Change name to Continue
+
+		// Stop if cycle number is higher than 80
+		return g.Cycle < 80 &&
+			// or if there were no update for at least 20 cycles
+			g.Cycle-g.LastUpdate < 20 &&
+			// or if
+			g.Best.Fitness < float64(len(g.Best.Alleles))
+	}
+
+	algorithm.Select = func(p *Population) Population {
+		parentNumber := 2
+
+		if parentNumber > len((*p)) || parentNumber > cap((*p)) {
+			return (*p)[:len((*p))]
+		}
+
+		return (*p)[:parentNumber]
+	}
+
+	algorithm.Cross = func(p *Population) Population {
+		childrenNumber := 2
+
+		if childrenNumber > len((*p)) || childrenNumber > cap((*p)) {
+			return (*p)[:len((*p))]
+		}
+
+		return (*p)[:childrenNumber]
+	}
+
+	algorithm.Mutation = func(c *Chromosome) {
+		flips := 3
+
+		if len(c.Alleles) <= flips {
+			panic("Not enough elements to flip")
+		}
+
+		indexes := make([]int, len(c.Alleles))
+		for index := range indexes {
+			indexes[index] = index
+		}
+
+		rand.Shuffle(len(indexes), func(i, j int) {
+			indexes[i], indexes[j] = indexes[j], indexes[i]
+		})
+
+		picks := indexes[:flips+1]
+
+		for _, pick := range picks {
+			c.Alleles[pick] = !c.Alleles[pick]
+		}
+	}
+
+	algorithm.Survive = func(p *Population) {
+		fmt.Println("Survival method has yet to be implemented")
+	}
+
+	algorithm.Run()
 }
